78070680aa43dd745d7a44cc811bbbb986181c34
Joel Woodfield <joelwoodfield@gmail.com>
2025-12-15 11:17:39 +1000

    Add basic css and use more meaningful names for bars

--- frontend/src/StatusBar.jsx
+++ frontend/src/StatusBar.jsx
@@ -2,9 +2,9 @@ export default function StatusBar({ stepData, connected }) {
   return (
     <div className="status-bar">
       <h4>Status: {connected ? "Connected" : "Disconnected" }</h4>
-      <p>Current Step: {stepData?.stepIdx}</p>
-      <p>Current Episode Return: {stepData?.episodeReturn}</p>
-      <p>Done: {stepData?.done ? "Yes" : "No"}</p>
+      <h4>Current Step: {stepData?.stepIdx}</h4>
+      <h4>Current Episode Return: {stepData?.episodeReturn}</h4>
+      <h4>Done: {stepData?.done ? "Yes" : "No"}</h4>
     </div>
   )
 }
