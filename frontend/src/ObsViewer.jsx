6c863e7302b973af38662c111f076d134669668b
Joel Woodfield <joelwoodfield@gmail.com>
2025-12-16 10:21:02 +1000

    Make observations overflow when too long

--- frontend/src/ObsViewer.jsx
+++ frontend/src/ObsViewer.jsx
@@ -3,11 +3,13 @@ export default function ObsViewer({ obs }) {
     <div className="obs-viewer">
       <h3>Observations</h3>
 
-      {obs?.map((value, index) => (
-        <div key={index}>
-          {index}: {value}
-        </div>
-      ))}
-    </div>
+      <div className="obs-list">
+        {obs?.map((value, index) => (
+          <div key={index} className="obs-item">
+            {index}: {value}
+          </div>
+        ))}
+      </div>
+   </div>
   )
 }
